10c3d92504549c4d727ea1300e4f17f524866f08
Ali Azadi <aliazadi8494@gmail.com>
2021-09-08 14:05:56 +0430

    Made some comments for the "functions.py" file and fixed the size problems and renamed some of the variables

--- functions.py
+++ functions.py
@@ -8,43 +8,28 @@ from PIL import ImageTk, Image
 from ttkthemes import ThemedTk
 from audios import audios, paths, playing_audios
 
-from time import sleep
-
-
 SCREEN = ThemedTk(themebg=True)
-SCREEN.config(bg='gray13')
-
-<<<<<<< HEAD
-screen = ThemedTk(theme="default")
-def open_music_player() :
-    
-    screen.wm_title('music player')
-    icon = PhotoImage(file="C:\\Users\\User\\Downloads\\music_player_logo.png")
-    screen.iconphoto(False, icon)
-    pygame.mixer.init()
-=======
 STYLE = ttk.Style()
-STYLE.configure('BW.TButton', font=('TkIconFont', 13, 'bold'), foreground='#116562', background='gray')
->>>>>>> functions
-
 BG_IMAGE_PATH = "C:\\Users\\User\\Desktop\\wallpaper(copy).png"
-IMAGE = ImageTk.PhotoImage(Image.open(BG_IMAGE_PATH))
-
 ICON = PhotoImage(file="C:\\Users\\User\\Desktop\\beat-music-player-2013-04-22.png")
-SCREEN.iconphoto(False, ICON)
-
 DEFAULT_VOLUME = 50
-IS_PAUSED = False
+is_paused = False
 
 
+# Setting the title, the style,the icon and the background image
+STYLE.configure('BW.TButton', font=('TkIconFont', 13, 'bold'), foreground='#116562', background='gray')
+IMAGE = ImageTk.PhotoImage(Image.open(BG_IMAGE_PATH))
+SCREEN.iconphoto(False, ICON)
 SCREEN.wm_title('music_player')
 panel = Label(SCREEN, image = IMAGE)
 panel.place(x=-5, y=-10)
 pygame.mixer.init()
 
 
+# The main function that is being called for each audio button
 def play_the_audio(audio) :
 
+    # Finding the audio in the list of paths
     for path in paths:
         if audio in path:
             audio = path
@@ -52,26 +37,26 @@ def play_the_audio(audio) :
     for widget in SCREEN.winfo_children():
         if 'scale' in str(widget):
             widget.set(1000)  
-            # stop the audio when the song is nearly over.
-            # make_play_button()
-
 
     pygame.mixer.music.load(open(audio, 'rb'))
 
+
+    # Creating a label to show the audio position
     def show_music_pos(pos):
 
         delta = timedelta(seconds=pos)
         delta_label = Label(SCREEN, text=str(delta), fg='#000066', bg='#b3b3b3', width=8)
         delta_label.place(x=220,y=580)
 
+
+    # This function keeps track of the current audio position and play the audio from that particular position
     def change_pos(current_pos):
 
         current_pos = int(current_pos)
-        print(current_pos)
         pygame.mixer.music.play(start=current_pos)
         show_music_pos(current_pos)
 
-
+    # Creating the music slider
     audio_length = pygame.mixer.Sound(audio).get_length()
     position_slider = Scale(SCREEN, from_=0, to=audio_length, sliderrelief='flat', 
     orient='horizontal', troughcolor='black', length=400,
@@ -81,12 +66,15 @@ def play_the_audio(audio) :
     position_slider.place(x=55, y=550)
 
 
+    # Adding to the position slider's position each second if the audio is playing
     def increase_pos():
 
-        if IS_PAUSED == False:
+        if is_paused == False:
             position_slider.set(position_slider.get() + 1)
             SCREEN.after(1000, increase_pos)
+
     increase_pos()
+
 
     def change_the_volume(volume):
 
@@ -103,40 +91,29 @@ def play_the_audio(audio) :
         scale_widget.set(DEFAULT_VOLUME)
         scale_widget.place(x=445, y=240)
 
-
     make_volume_scale()
 
-            
-
-
-
-
-
-
-
-
-    
 
     def make_play_button() :
   
         def unpause_music():
 
-            global IS_PAUSED
-            IS_PAUSED = False
+            global is_paused
+            is_paused = False
             pygame.mixer.music.unpause()
             make_stop_button()
             increase_pos()
             
-
         resume = ttk.Button(SCREEN, text='▶',style='BW.TButton' ,command=unpause_music)
         resume.place(x=185, y=510)
+
 
     def make_stop_button() :
 
         def pause_music():
             
-            global IS_PAUSED
-            IS_PAUSED = True
+            global is_paused
+            is_paused = True
             pygame.mixer.music.pause()
             make_play_button()
 
@@ -146,7 +123,8 @@ def play_the_audio(audio) :
     
     make_stop_button()
 
-# Creating widgets for songs and bringing them onto the SCREEN
+
+# Creating widgets for the audio and bringing them onto the SCREEN
 def make_buttons(window,files) :
     global current_time
 
@@ -154,50 +132,45 @@ def make_buttons(window,files) :
         audio_1 = Button(window,text=files[0],height=4,width=40,bg='#31314f',fg='white',command=lambda : play_the_audio(files[0]))
         audio_1.config(font='TkIconFont')
         audio_1.place(x=90,y=190)
-
-    
     except :
-        pass
+        raise Exception("It's not possible to play this audio!")
 
     try :
         audio_2 = Button(window,text=files[1],height=4,width=40,bg='#31314f',fg='white',command=lambda : play_the_audio(files[1]))
         audio_2.config(font='TkIconFont')
         audio_2.place(x=90,y=270)
-
     except :
-        pass
+        raise Exception("It's not possible to play this audio!")
 
     try :
         audio_3 = Button(window,text=files[2],height=4,width=40,bg='#31314f',fg='white',command=lambda : play_the_audio(files[2]))
         audio_3.config(font='TkIconFont')
         audio_3.place(x=90,y=350)
     except :
-        pass
+        raise Exception("It's not possible to play this audio!")
 
     try :
         audio_4 = Button(window,text=files[3],height=4,width=40,bg='#31314f',fg='white',command=lambda : play_the_audio(files[3]))
         audio_4.config(font='TkIconFont')
         audio_4.place(x=90,y=430)
     except :
-        pass
+        raise Exception("It's not possible to play this audio!")
 
 
 
 
-    # Creating another window either the "next page" button is clicked or the "previous page" button
+    # Creating another window either the "next page" button or the "previous page" button is clicked
     def open_another_window() :
 
         
-        for i in SCREEN.winfo_children() :
-            i.destroy()
+        for widget in SCREEN.winfo_children() :
+            widget.destroy()
         panel = Label(SCREEN, image = IMAGE)
         panel.place(x=-5, y=-10)
-        if pygame.mixer.music.get_busy() :
-            pygame.mixer.music.pause()
-        for i in audios  :
-            if files[len(files) - 1] in i :
+        for audio in audios  :
+            if files[len(files) - 1] in audio :
                 try :
-                        make_buttons(SCREEN,audios[audios.index(i)-1]) 
+                        make_buttons(SCREEN,audios[audios.index(audio)-1]) 
                 except :
                     pass
                 
@@ -211,9 +184,8 @@ def make_buttons(window,files) :
 
 def create_SCREEN() :
 
-    SCREEN.geometry("500x630")
+    SCREEN.geometry("490x626")
     SCREEN.resizable(False, False)
-
     make_buttons(SCREEN,playing_audios)
     SCREEN.mainloop()
 
--- main.py
+++
@@ -1,8 +0,0 @@
-from functions import open_music_player
-
-
-if __name__ == '__main__':
-    
-    open_music_player()
-
-
